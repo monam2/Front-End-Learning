31e67744fda6f294f81c400d67317d5fb8abdb09
김모남 <138136517+monam2@users.noreply.github.com>
2023-07-31 19:43:21 +0900

    Update App.js

--- Learning React/App.js
+++ Learning React/App.js
@@ -1,35 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
-function Header() {
+function Header(props) {
   return <header>
-    <h1><a href="/">React</a></h1>
+    <h1><a href="/">{props.title}</a></h1>
   </header >
 
 }
-function Nav() {
+function Nav(props) {
+  const lis = [];
+  for (let i = 0; i < props.topics.length; i++) {
+    let t = props.topics[i];
+    lis.push(<li key={t.id}><a href={'/read/'+t.id}>{t.title}</a></li>)
+  }
   return (<nav>
     <ol>
-      <li><a href="/read/1">html</a></li>
-      <li><a href="/read/2">css</a></li>
-      <li><a href="/read/3">js</a></li>
+      {lis}
     </ol>
   </nav>)
 }
-function Article() {
+function Article(props) { //props의 속성은 obj
   return <article>
-    <h2>Welcome</h2>
-    Hello, WEB!
+    <h2>{props.title}</h2>
+    {props.body}
   </article>
 }
 
 function App() {
+  const topics = [
+    { id: 1, title: 'html', body: 'html is ...' },
+    { id: 2, title: 'css', body: 'css is ...' },
+    { id: 3, title: 'js', body: 'js is ...' }
+  ]
   return (
     <div className="App">
+      <Header title='WEB'></Header>
       <Header></Header>
-      <Header></Header>
-      <Article></Article>
-      <Nav></Nav>
+      <Article title="welcome" body="Hello, Web"></Article>
+      <Nav topics={topics}></Nav>
     </div>
   );
 }
