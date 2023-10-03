534b9ca99b47dee3c023b510bfe80a0324bf7a12
모남 <138136517+monam2@users.noreply.github.com>
2023-10-03 22:59:00 +0900

    Update response_check.js

--- Vanila JS/Response_check/response_check.js
+++ Vanila JS/Response_check/response_check.js
@@ -3,6 +3,7 @@ const $result = document.querySelector('#result');
 
 let startTime; //시간 기록
 let endTime;
+const records = []; //평균 반속을 구하기 위해 각 속도를 저장할 배열
 
 $screen.addEventListener('click',(e)=>{
     if (e.target.classList.contains('waiting')) { //파란색(대기) 일때
@@ -26,10 +27,18 @@ $screen.addEventListener('click',(e)=>{
 
     } else if (e.target.classList.contains('now')) { //초록색 일때
         //끝 시간 재기 & 시간 차이를 표시(반응속도)
-        endTime = new Date();
-        $result.textContent = `${endTime - startTime}ms`;
+        endTime = new Date(); //안하면 undefined 뜸.
+        const current = endTime - startTime;
+        records.push(current);
+        const average = records.reduce((a,c)=>a+c)/records.length; //reduce사용해서 전체합.. 평균 구하기.
+
+        console.log(startTime, endTime, records)
+        $result.textContent = `현재 ${current}ms, 평균 ${parseInt(average)}ms`;
+        startTime = null; //왜지움?
+        endTime = null; // -> n회차의 반응속도를 배열에 기록&출력.. 비워주지 않으면 이전에 썼던 값이 남아있을 수도 있음.
+
         $screen.classList.remove('now'); //다시 대기 상태로 들어가야함.
-        $screen.classList.add('ready');
+        $screen.classList.add('waiting');
         $screen.textContent = '클릭해서 시작하세요.';
 
     }
