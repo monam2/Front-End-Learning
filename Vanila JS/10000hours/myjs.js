c41c2f4196303f85534a824e3b8b0de050094c10
모남 <138136517+monam2@users.noreply.github.com>
2023-11-24 21:26:35 +0900

    Update myjs.js

--- Vanila JS/10000hours/myjs.js
+++ Vanila JS/10000hours/myjs.js
@@ -10,18 +10,18 @@ const $body = document.querySelector("body");
 
 function calculator() {
     const $fieldValue = document.querySelector("#field_value");
-    let $timeValue = document.querySelector("#time_value");
-    let $timeValue_int = Number($timeValue.value);
+    const $timeValue = document.querySelector("#time_value");
+    const $timeValue_int = Number($timeValue.value);
     
     const $fieldResult = document.querySelector(".field_result");
     const $timeResult = document.querySelector(".time_result");
 
     if ($fieldValue.value == '') {
-        alert("입력되지 않았습니다.");
+        alert("분야가 입력되지 않았습니다.");
         $fieldValue.focus();
         return false
     } else if ($timeValue.value == '') {
-        alert("입력되지 않았습니다.");
+        alert("시간이 입력되지 않았습니다.");
         $timeValue.focus();
         return false;
     } else if ($timeValue_int >24) {
@@ -33,10 +33,10 @@ function calculator() {
     $loading.style.display = 'flex';
 
     setTimeout(() => {
-        $loading.style.display = 'none';
-        $result.style.display = 'flex';
         $fieldResult.innerHTML = $fieldValue.value;
         $timeResult.innerHTML = parseInt(10000/($timeValue_int), 10);
+        $loading.style.display = 'none';
+        $result.style.display = 'flex';
     }, 1800);
 }
 
